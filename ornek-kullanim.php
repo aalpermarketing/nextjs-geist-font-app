0983577027f0f170f3d291726a13569cfb8f5d8a
aalpermarketing <aalpermarketing@gmail.com>
2025-06-22 06:04:20 +0300

    Delete ornek-kullanim.php

--- ornek-kullanim.php
+++
@@ -1,7 +0,0 @@
-// API URL ve API anahtarını tanımla
-$api_url = 'https://anasite.com/api/fetch.php'; // Ana sitenin API URL'si
-$api_key = 'YOUR_API_KEY_HERE'; // API anahtarınız
-
-// cURL ile API'den veri çekme
-function fetchFromAPI($url, $api_key, $kategori = 'all') {
-    $ch = curl_init();
